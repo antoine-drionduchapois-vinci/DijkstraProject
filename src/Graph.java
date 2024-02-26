fe29eb45793812245634f2da9547a395875af05c
Bilic <flaviu.bilic@student.vinci.be>
2024-02-26 14:25:31 +0100

    Added: Graph file, constructor and 2 methods not implemented yet

---
+++ src/Graph.java
@@ -0,0 +1,16 @@
+import java.io.File;
+
+public class Graph {
+
+    public Graph(File file1, File file2){
+
+    }
+
+    public Void calculerItineraireMinimisantNombreRoutes(String city1,String city2){
+
+    }
+
+    public Void calculerItineraireMinimisantKm(String city1, String city2){
+
+    }
+}
